38a39c676bd0b2c74c86ca93805d8ab066e044c1
Kristy Gislason <kristygislason@gmail.com>
2022-10-08 17:29:27 -0700

    Convert functions to methods inside the task object

--- index.js
+++ index.js
@@ -1,23 +1,21 @@
 // Create a new task by adding to the arrays
 // A new task will be created as incomplete
-function newTask(title, description) {
+const newTask = function(title, description) {
   const task = {
     title: title,
     description: description,
-    complete: false
+    complete: false,
+
+    logState: function() {
+      console.log(`${this.title} has${this.complete ? " " : " not "}been completed`);
+    },
+
+    markCompleted: function() {
+      this.complete = true;
+    }
   };
   return task;
-}
-
-// Mark a task as complete by setting the task's status in the `taskComplete` array to `true`
-function completeTask(task) {
-  task.complete = true;
-}
-
-// Print the state of a task to the console in a nice readable way
-function logTaskState(task) {
-  console.log(`${task.title} has${task.complete ? " " : " not "}been completed`);
-}
+};
 
 // DRIVER CODE BELOW
 
@@ -25,7 +23,7 @@ const task1 = newTask("Clean Cat Litter", "Take all the 💩 out of the litter b
 const task2 = newTask("Do Laundry", "😨");
 const tasks = [task1, task2];
 
-logTaskState(task1); // Clean Cat Litter has not been completed
-completeTask(task1);
-logTaskState(task1); // Clean Cat Litter has been completed
+task1.logState(task1); // Clean Cat Litter has not been completed
+task1.markCompleted(task1);
+task1.logState(task1); // Clean Cat Litter has been completed
 console.log(tasks);
